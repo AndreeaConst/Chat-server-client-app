2aaa774866abc682bc04ade15279029b9dcea269
Catalina <catalina.g1808@gmail.com>
2021-05-30 00:09:27 +0300

    Added the client

---
+++ ChatClient/ChatClient/ChatServiceClient.cs
@@ -0,0 +1,61 @@
+﻿using ChatClient;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GrpcWpfSample.Client
+{
+    public class ChatServiceClient
+    {
+        private readonly Chat.ChatClient m_client;
+
+        public ChatServiceClient()
+        {
+            // Locate required files and set true to enable SSL
+            var secure = false;
+
+            if (secure)
+            {
+                // create secure channel
+                var serverCACert = File.ReadAllText(@"C:\localhost_server.crt");
+                var clientCert = File.ReadAllText(@"C:\localhost_client.crt");
+                var clientKey = File.ReadAllText(@"C:\localhost_clientkey.pem");
+                var keyPair = new KeyCertificatePair(clientCert, clientKey);
+                var credentials = new SslCredentials(serverCACert, keyPair);
+
+                // Client authentication is an option. You can remove it as follows if you only need SSL.
+                //var credentials = new SslCredentials(serverCACert);
+
+                m_client = new Chat.ChatClient(
+                    new Grpc.Core.Channel("localhost", 50052, credentials));
+            }
+            else
+            {
+                // create insecure channel
+                m_client = new Chat.ChatClient(
+                    new Grpc.Core.Channel("localhost", 50052, ChannelCredentials.Insecure));
+            }
+        }
+
+        public async Task Write(ChatLog chatLog)
+        {
+            await m_client.WriteAsync(chatLog);
+        }
+
+        public IAsyncEnumerable<ChatLog> ChatLogs()
+        {
+            var call = m_client.Subscribe(new Empty());
+
+            // I do not want to expose gRPC such as IAsyncStreamReader or AsyncServerStreamingCall.
+            // I also do not want to bother user of this class with asking to dispose the call object.
+
+            return call.ResponseStream
+                .ToAsyncEnumerable()
+                .Finally(() => call.Dispose());
+        }
+    }
+}
--- ChatClient/ChatClient/Program.cs
+++ ChatClient/ChatClient/Program.cs
@@ -1,27 +1,68 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Net.Client;
+using GrpcWpfSample.Client;
 
 namespace ChatClient
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             // The port number(5001) must match the port of the gRPC server.
 
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
-            var client = new Chat.ChatClient(channel);
+            //using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            //var client = new Chat.ChatClient(channel);
 
-            /*var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+            Console.Write("Enter your name> ");
+            var name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = "anonymous";
+            }
 
-            Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();*/
+            Console.WriteLine($"Joined as {name}");
+
+            var chatServiceClient = new ChatServiceClient();
+            var consoleLock = new object();
+
+            // subscribe (asynchronous)
+            _ = chatServiceClient.ChatLogs()
+                .ForEachAsync((x) =>
+                {
+                    // if the user is writing something, wait until it finishes.
+                    lock (consoleLock)
+                    {
+                        Console.WriteLine($"{x.Time.ToDateTime().ToString("HH:mm:ss")} {x.Name}: {x.Content}");
+                    }
+                });
+
+            // write
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                // A key input starts writing mode
+                lock (consoleLock)
+                {
+                    var content = key.KeyChar + Console.ReadLine();
+
+                    chatServiceClient.Write(new ChatLog
+                    {
+                        Name = name,
+                        Content = content,
+                        Time = Timestamp.FromDateTime(DateTime.Now.ToUniversalTime()),
+                    }).Wait();
+                }
+            }
+
         }
     }
-}
+
+
+    }

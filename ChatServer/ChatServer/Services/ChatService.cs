feb6ae6ac26a3afd6b359d9285f2185966dafabd
Catalina <catalina.g1808@gmail.com>
2021-05-31 12:55:20 +0300

    Refactored the server

--- ChatServer/ChatServer/Services/ChatService.cs
+++ ChatServer/ChatServer/Services/ChatService.cs
@@ -24,7 +24,7 @@ namespace ChatServer.Server.Services
             private ChatServiceModel m_chatService = null;
             private readonly Empty m_empty = new Empty();
             [Import]
-            private UsersServiceModel m_usersService=null;
+            private UsersServiceModel m_usersService = null;
 
             private const int Port = 50052;
             private readonly Grpc.Core.Server m_server;
@@ -32,50 +32,19 @@ namespace ChatServer.Server.Services
             public ChatServiceGrpcServer()
             {
 
-                // Locate required files and set true to enable SSL
-                var secure = false;
-
-                if (secure)
+                m_server = new Grpc.Core.Server
                 {
-                    // secure
-                    var clientCACert = File.ReadAllText(@"C:\localhost_client.crt");
-                    var serverCert = File.ReadAllText(@"C:\localhost_server.crt");
-                    var serverKey = File.ReadAllText(@"C:\localhost_serverkey.pem");
-                    var keyPair = new KeyCertificatePair(serverCert, serverKey);
-                    var credentials = new SslServerCredentials(new[] { keyPair }, clientCACert, SslClientCertificateRequestType.RequestAndRequireAndVerify);
-
-                    // Client authentication is an option. You can remove it as follows if you only need SSL.
-                    //var credentials = new SslServerCredentials(new[] { keyPair });
-
-                    m_server = new Grpc.Core.Server
-                    {
-                        Services =
+                    Services =
                     {
                         Chat.BindService(this)
                             .Intercept(new IpAddressAuthenticator())
                     },
-                        Ports =
-                    {
-                        new ServerPort("localhost", Port, credentials)
-                    }
-                    };
-                }
-                else
-                {
-                    // insecure
-                    m_server = new Grpc.Core.Server
-                    {
-                        Services =
-                    {
-                        Chat.BindService(this)
-                            .Intercept(new IpAddressAuthenticator())
-                    },
-                        Ports =
+                    Ports =
                     {
                         new ServerPort("localhost", Port, ServerCredentials.Insecure)
                     }
-                    };
-                }
+                };
+
             }
 
             public void Start()
@@ -85,21 +54,14 @@ namespace ChatServer.Server.Services
                 m_logger.Info("Started.");
             }
 
-            public override async Task Subscribe(Username request, IServerStreamWriter<ChatLog> responseStream, ServerCallContext context) //
+            public override async Task Subscribe(Username request, IServerStreamWriter<ChatLog> responseStream, ServerCallContext context)
             {
-                var peer = context.Peer; 
-                m_logger.Info($"{request} subscribes."); //modif
+                var peer = context.Peer;
+                m_logger.Info($"{request} subscribes.");
                 m_usersService.Add(request);
 
-                context.CancellationToken.Register(() => m_logger.Info($"{request} cancels subscription.")); //modif
+                context.CancellationToken.Register(() => m_logger.Info($"{request} cancels subscription."));
                 context.CancellationToken.Register(() => m_usersService.Delete(request));
-
-                // Completing the method means disconnecting the stream by server side.
-                // If subscribing IObservable, you have to block this method after the subscription.
-                // I prefer converting IObservable to IAsyncEnumerable to consume the sequense here
-                // because gRPC interface is in IAsyncEnumerable world.
-                // Note that the chat service model itself is in IObservable world
-                // because chat is naturally recognized as an event sequence.
 
                 try
                 {
@@ -115,21 +77,21 @@ namespace ChatServer.Server.Services
             }
 
 
-            public override async Task GetUsers(Empty request, IServerStreamWriter<Username> responseStream, ServerCallContext context) //
+            public override async Task GetUsers(Empty request, IServerStreamWriter<Username> responseStream, ServerCallContext context)
             {
-               
-                if(m_usersService!=null)
-                try
-                {
-                    await m_usersService.GetUsersAsObservable()
-                        .ToAsyncEnumerable()
-                        .ForEachAwaitAsync(async (x) => await responseStream.WriteAsync(x))
-                        .ConfigureAwait(false);
-                }
-                catch (TaskCanceledException)
-                {
-                   
-                }
+
+                if (m_usersService != null)
+                    try
+                    {
+                        await m_usersService.GetUsersAsObservable()
+                            .ToAsyncEnumerable()
+                            .ForEachAwaitAsync(async (x) => await responseStream.WriteAsync(x))
+                            .ConfigureAwait(false);
+                    }
+                    catch (TaskCanceledException)
+                    {
+
+                    }
             }
 
             public override Task<Empty> Write(ChatLog request, ServerCallContext context)

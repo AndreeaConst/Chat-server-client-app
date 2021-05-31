9e8f0983207c6dc771b24c6acfab1573215794ba
Catalina <catalina.g1808@gmail.com>
2021-05-31 13:13:52 +0300

    Added some files

--- ChatClient/ChatClientConsole/IAsyncStreamReaderExtensions.cs
+++ ChatClient/ChatClientConsole/IAsyncStreamReaderExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GrpcWpfSample.Client
+namespace ChatClientConsole
 {
     public static class IAsyncStreamReaderExtensions
     {
